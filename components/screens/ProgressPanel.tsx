9fe3f4a982bbf328ee51ec74ef700427a0596c13
jim wen <jim.wen@monash.edu>
2025-11-09 01:56:15 +1100

    Adjust Progress Panel Layout

--- components/screens/ProgressPanel.tsx
+++ components/screens/ProgressPanel.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle, CardContent, CardDescription } from "@/components/ui/card";
@@ -20,9 +20,12 @@ import { fetchActionPlanRequest } from "@/lib/authApi";
 import { ActionPlanResponse } from "@/types/ActionPlan";
 import { ScrollArea } from "../ui/scroll-area";
 
+type FilterType = "all" | "completed" | "none";
+
 export function ProgressPanel() {
   const { authorizedFetch } = useAuth();
   const [data, setData] = useState<ActionPlanResponse>({ action_plan: [] });
+  const [filter, setFilter] = useState<FilterType>("all");
 
   const load = async () => {
     try {
@@ -36,6 +39,20 @@ export function ProgressPanel() {
   useEffect(() => {
     load();
   }, []);
+
+  const filteredData = useMemo(() => {
+    switch (filter) {
+      case "completed":
+        return data.action_plan.filter(
+          (item) => item.status === "Completed timely!" || item.status === "Completed late."
+        );
+      case "none":
+        return data.action_plan.filter((item) => item.status === null);
+      default:
+        // all
+        return data.action_plan;
+    }
+  }, [data.action_plan, filter]);
 
   return (
     <div className="space-y-6">
@@ -54,11 +71,12 @@ export function ProgressPanel() {
             </em>
           </p>
 
-          <div className="flex justify-end mt-2">
+          <div className="flex justify-end my-4 gap-4">
             <Button
               variant="outline"
               size="sm"
-              className="flex items-center gap-2"
+              className="flex items-center gap-2 cursor-pointer"
+              onClick={()=> setFilter("none")}
             >
               <ChevronsRightIcon className="h-4 w-4" />
               <span>In-progress items only</span>
@@ -66,7 +84,8 @@ export function ProgressPanel() {
             <Button
               variant="outline"
               size="sm"
-              className="flex items-center gap-2"
+              className="flex items-center gap-2 cursor-pointer"
+              onClick={()=> setFilter("completed")}
             >
               <CircleCheckBigIcon className="h-4 w-4" />
               <span>Completed items only</span>
@@ -74,25 +93,57 @@ export function ProgressPanel() {
             <Button
               variant="outline"
               size="sm"
-              className="flex items-center gap-2"
+              className="flex items-center gap-2 cursor-pointer"
+              onClick={()=> setFilter("all")}
             >
               <Rows4Icon className="h-4 w-4" />
               <span>All items</span>
             </Button>
           </div>
 
-          <ScrollArea className="h-[48rem] rounded-md border flex mt-4">
-            <div className="flex w-full max-w-md flex-col gap-4 mt-2 px-2">
-              {data.action_plan.map(action => (
-                <Item key={action.id} variant="outline">
+          <ScrollArea className="h-[44rem] rounded-md border flex mt-4">
+            <div className="flex w-full flex-col gap-4 mt-2 px-2">
+              {filteredData.map(action => (
+                <Item
+                  key={action.id}
+                  variant="outline"
+                  className={[
+                    action.status === "Completed timely!" ? "bg-green-50" :
+                      action.status === "Completed late." ? "bg-slate-50" : ""
+                  ].join(" ")}
+                >
                   <ItemContent>
-                    <ItemTitle>{action.action_content}</ItemTitle>
-                    <ItemDescription>
-                      <em>To be completed by: {action.target_completion_date}</em>
-                    </ItemDescription>
-                    <ItemDescription>
-                      <em>Status: {action.status} (updated on: {(new Date(action.marked_status_time * 1000)).toLocaleDateString()})</em>
-                    </ItemDescription>
+                    {!action.status ? (
+                      <div className="flex">
+                        <ItemTitle>{action.action_content}</ItemTitle>
+                        <div className="flex flex-col items-center justify-center gap-2">
+                          <Button
+                            variant="outline"
+                            size="sm"
+                            className="flex items-center gap-2 bg-green-50 border cursor-pointer"
+                          >
+                            <span>Completed timely!</span>
+                          </Button>
+                          <Button
+                            variant="outline"
+                            size="sm"
+                            className="flex items-center gap-2 cursor-pointer"
+                          >
+                            <span>Completed late.</span>
+                          </Button>
+                        </div>
+                      </div>
+                    ): (
+                      <>
+                        <ItemTitle>{action.action_content}</ItemTitle>
+                        <ItemDescription>
+                          <em>To be completed by: {action.target_completion_date}</em>
+                        </ItemDescription>
+                        <ItemDescription>
+                          <em>Status: {action.status} (updated on: {(new Date(action.marked_status_time * 1000)).toLocaleDateString()})</em>
+                        </ItemDescription>
+                      </>
+                    )}
                   </ItemContent>
                 </Item>
               ))}
