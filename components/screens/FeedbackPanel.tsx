9ef55d385d87ed4366115cf6a4b405786d607782
jim wen <jim.wen@monash.edu>
2025-11-18 20:54:58 +1100

    Adjust Feedback Panel Layout

--- components/screens/FeedbackPanel.tsx
+++ components/screens/FeedbackPanel.tsx
@@ -1,9 +1,11 @@
 import { useCallback, useMemo, useState } from "react";
+import "@/app/globals.css";
 
+import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle, CardContent, CardDescription, CardFooter } from "@/components/ui/card";
+import { Card, CardAction, CardHeader, CardTitle, CardContent, CardDescription, CardFooter } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { ChevronLeft, ChevronRight, GripVertical  } from "lucide-react";
+import { ChevronLeft, ChevronRight, GripVertical, Lightbulb } from "lucide-react";
 
 import { Feedback, FeedbackSet } from "@/types/Feedback"; // Import type for feedback
 
@@ -37,17 +39,20 @@ function FeedbackCard({
     <div className={className}>
       <Card>
         <CardHeader>
-          <CardTitle className="text-xl font-medium">How you should improve?</CardTitle>
+          <CardAction className="col-start-1 row-span-2 row-start-1 justify-self-center self-center">
+            <Lightbulb />
+          </CardAction>
+          <CardTitle className="font-thin lg:pr-16">Feedback summary</CardTitle>
+          <CardDescription className="text-lg font-semibold text-black lg:pr-16">How you should improve?</CardDescription>
         </CardHeader>
         <CardContent>
-          <CardDescription className="text-lg">
-            <em>{feedback.feedback}</em>
+          <Badge variant="secondary" className="py-2">Your Latest Feedback</Badge>
+          <CardDescription className="text-lg font-normal text-black font-sans italic pt-2">
+            {feedback.feedback}
           </CardDescription>
 
-          <p className="text-base text-muted-foreground mt-2">
-            <em>
+          <p className="text-base text-muted-foreground mt-2 italic">
             To better achieve the learning outcomes for Weeks 0 - {currentWeek}, here are some suggestions that you may find helpful:
-            </em>
           </p>
 
           <ScrollArea className="h-72 rounded-md border flex mt-4">
@@ -60,7 +65,7 @@ function FeedbackCard({
                     className="flex items-center justify-between rounded-sm border hover:bg-slate-50 text-sm"
                     onDragStart={(e) => handleDragStart(e, index, feedback.actionable_advice)}
                   >
-                    <span className="m-4">{advice}</span>
+                    <span className="m-4 font-sans">{advice}</span>
                     <div className="min-width-40 cursor-grab m-4">
                       <GripVertical />
                     </div>
@@ -70,10 +75,8 @@ function FeedbackCard({
             </div>
           </ScrollArea>
 
-          <p className="text-base text-muted-foreground mt-4">
-            <em>
-              To support your achievement of the learning outcomes for Week {currentWeek + 1}, here are some suggestions:
-            </em>
+          <p className="text-base text-muted-foreground italic mt-4">
+            To support your achievement of the learning outcomes for Week {currentWeek + 1}, here are some suggestions:
           </p>
 
           <ScrollArea className="h-72 rounded-md border flex mt-4">
