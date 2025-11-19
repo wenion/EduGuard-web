0cb7829b90025eda07465fe2cfeb28182fe66a4a
jim wen <jim.wen@monash.edu>
2025-11-19 18:13:59 +1100

    Remove unuseful next-auth

--- components/screens/HomeView.tsx
+++ components/screens/HomeView.tsx
@@ -1,6 +1,5 @@
 "use client";
 import type { Dispatch, SetStateAction } from "react";
-import { useSession } from "next-auth/react";
 type Screen = "home" | "dashboard" | "settings";
 
 export default function HomeView({
