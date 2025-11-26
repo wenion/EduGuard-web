ff24f24373da96e7fbf46c2e1050a6eb8f3f6106
jim wen <jim.wen@monash.edu>
2025-11-26 15:46:34 +1100

    Admin label

--- components/screens/LoginView.tsx
+++ components/screens/LoginView.tsx
@@ -7,7 +7,7 @@ export default function LoginView() {
     <div className="flex flex-col items-center justify-center mt-10 space-y-6">
       <p>Welcome to Edvance! We are glad to see you here! Please log in first!</p>
       <LoginForm />
-      <div>Don't have an account yet or forget your password? Contact <a href="mailto:guanliang.chen@example.com" className="text-blue-600 underline">Guanliang Chen</a></div>
+      <div>Don't have an account yet or forget your password? Contact <a href="mailto:guanliang.chen@example.com" className="text-blue-600 underline">an administrator</a></div>
     </div>
   );
 }
