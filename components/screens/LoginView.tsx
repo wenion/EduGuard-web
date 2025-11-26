e758039811e9f911035199985af6acd514393659
jim wen <jim.wen@monash.edu>
2025-11-26 12:51:50 +1100

    Center the LoginForm

--- components/screens/LoginView.tsx
+++ components/screens/LoginView.tsx
@@ -4,10 +4,10 @@ import LoginForm from "./LoginForm";
 
 export default function LoginView() {
   return (
-    <>
+    <div className="flex flex-col items-center justify-center mt-10 space-y-6">
       <p>Welcome to Edvance! We are glad to see you here! Please log in first!</p>
       <LoginForm />
-      <div>Don't have an account yet or forget your password? Contact <a href="mailto:guanliang.chen@example.com">Guanliang Chen</a></div>
-    </>
+      <div>Don't have an account yet or forget your password? Contact <a href="mailto:guanliang.chen@example.com" className="text-blue-600 underline">Guanliang Chen</a></div>
+    </div>
   );
 }
